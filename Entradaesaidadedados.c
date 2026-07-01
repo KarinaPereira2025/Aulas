05c4192d3f441d0cdb73a3d8e192df8e62437d3b
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-01 13:38:03 +0000

    atualizacoes

---
+++ Entradaesaidadedados.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+
+    int idade; 
+    float altura;
+    double saldobancario;
+    char inicial;
+    //char nome[20] = "Bruno"; 
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    
+    printf("Digite seu saldobancario: \n");
+    scanf("%d", &saldobancario);
+
+   printf("digite a inicialdonome: \n");
+   scanf("%c", &inicial);
+    //printf("nome: %s \n", nome);
+
+    return 0;
+
+}
