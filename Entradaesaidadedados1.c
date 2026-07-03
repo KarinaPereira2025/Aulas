84b7d8f861536414d1fd540300cfd64c5e70ddc3
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-03 12:56:57 +0000

    atualizaooo

--- Entradaesaidadedados1.c
+++ Entradaesaidadedados1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-main(){
+int main(){
 
     int idade = 24;
     float altura = 1.70;
     double saldobancario = 234.56;
     char inicial = 'B';
     char nome[20] = "Bruno";
+    system("clear");
 
     printf("Idade: %d anos \n", idade);
-    printf("Altura: %2.f \n", altura);
-    printf("Saldobancario: %2.f  \n", saldobancario);
+    printf("Altura: %.2f \n", altura);
+    printf("Saldobancario: %.2f  \n", saldobancario);
     printf("Inicial %c  \n", inicial);
     printf("Nome: %s ", nome);
 
