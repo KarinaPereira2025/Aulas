edec1b64aea9bf8d88dd2a51340575f264fbc605
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-03 13:47:06 +0000

    atualizacao

---
+++ Entradaesaidadedados2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+    system("clear");
+
+    printf("Sua idade: %d \n");
+    scanf("%d", &idade);
+    printf("Sua altura: %.2f \n");
+    scanf("%f", &altura);
+    printf("Seu nome: %s \n");
+    scanf("%s",  &nome);
+    printf("Sua matricula: %d \n");
+    scanf("%d", &matricula);
+
+    return 0;
+
+
+}
