1b3895806bfa80c38f0c8d13e4e817107da12b59
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-07 10:36:22 +0000

    atualizao

---
+++ Entradaesaidadedados3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int maim(){
+    //operadores matematicos
+
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("Entre com o numero1: \n");
+    scanf("%d", &numero1);
+    printf("Entre com o numero2: \n");
+    scanf("%d", &numero2);
+
+    //Opereadores de soma
+    soma =  numero1 + numero2;
+    //Operadores de subtracao
+    subtracao = numero1 - numero2;
+    //Operadores de multiplicacao
+    multiplicacao = numero1 * numero2;
+    //Operadores de divisao
+    divisao = numero1 / numero2;
+
+    printf("A soma e: %d\n:", soma);
+    printf("A subtracao e: %d\n:", subtracao);
+    printf("A multiplicacao e: %d\n:", multiplicacao);
+    printf("A divisao e: %d\n:", divisao);
+
+}
