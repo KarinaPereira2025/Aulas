5124454376b3b0702e3641d8db2b0849bcd162a4
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-08 10:28:59 +0000

    atualizacao

--- Entradaesaidadedados4.c
+++ Entradaesaidadedados4.c
@@ -19,9 +19,12 @@ int main(){
    //resultado = resultado - numero1;
    resultado -= numero1;
    printf("Resultado: %d\n", resultado);
-
+   
+   //resultado = resultado * 5;
    resultado *= 5;
    printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado /2;
    resultado /= 2;
    printf("Resultado: %d\n", resultado);
 
