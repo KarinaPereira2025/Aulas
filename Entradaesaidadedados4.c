375faafba29ed3e5d4c0a0f2f725e6a3e149f281
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-08 09:49:39 +0000

    atualizacao

---
+++ Entradaesaidadedados4.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+int main(){
+    /*Atribuição simples(=)
+    Atribuição com soma(+=)
+    Atribuição com subtração(-=)
+    Atribuição com multiplicação(*=)
+    Atribuição com divisao(/=)
+    */
+   int numero1 = 10, numero2, resultado;
+   resultado = 10;
+   printf("Resultado: %d\n", resultado);
+
+   resultado += 20;
+   printf("Resultado: %d\n", resultado);
+
+   resultado -= numero1;
+   printf("Resultado: %d\n", resultado);
+
+
+
+
+
+}
