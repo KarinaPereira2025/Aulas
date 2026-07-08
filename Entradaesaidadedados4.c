2ecaa55afee491516ba214a4824ea64ef06d6059
KarinaPereira2025 <karinaff.pereira@gmail.com>
2026-07-08 10:21:23 +0000

    atualizacaoo

--- Entradaesaidadedados4.c
+++ Entradaesaidadedados4.c
@@ -7,15 +7,22 @@ int main(){
     Atribuição com divisao(/=)
     */
    int numero1 = 10, numero2, resultado;
-     system("clear");
+    system("clear");
   
    resultado = 10;
    printf("Resultado: %d\n", resultado);
 
+   //resultado = resultado + 20;
    resultado += 20;
    printf("Resultado: %d\n", resultado);
 
+   //resultado = resultado - numero1;
    resultado -= numero1;
+   printf("Resultado: %d\n", resultado);
+
+   resultado *= 5;
+   printf("Resultado: %d\n", resultado);
+   resultado /= 2;
    printf("Resultado: %d\n", resultado);
 
 
